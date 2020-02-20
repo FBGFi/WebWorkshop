2828f16339e78152aa8a3b494b73a09576204e50
n0kk0nen <57390863+n0kk0nen@users.noreply.github.com>
2020-02-20 12:50:24 +0200

    Update colors.js

--- react-app/SellGamesApp/constants/colors.js
+++ react-app/SellGamesApp/constants/colors.js
@@ -1,7 +1,7 @@
 export default {
-  yellow: 'R254 G209 B58', //#fed13a
-  red: 'R190 G58 B34', //#be3a22
-  blue: 'R34 G43 B111', //#222b6f 
-  black: '#000',
-  white: '#fff'
+  yellow: '#fed13a', 
+  red: '#be3a22', 
+  blue: '#222b6f', 
+  black: '#000000',
+  white: '#ffffff'
 };
