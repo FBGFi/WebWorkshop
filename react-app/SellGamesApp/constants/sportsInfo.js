f55e3e503452234dda7ceba07e2af7003e8058c5
Simo Wesa <simo.wesa@gmail.com>
2020-03-09 14:12:22 +0200

    Create sportsInfo.js
    Sports info constants

---
+++ react-app/SellGamesApp/constants/sportsInfo.js
@@ -0,0 +1,19 @@
+export default{
+    athletics:[{
+        title: "Organizers",
+        content: "LAB University of Applied Sciences together with Lahden Ahkera ry."  
+    },
+    {
+        title: "Time and place",
+        content: "-May 16  – 17, 2020\n-Lahti Sports Centre\n-Salpausselänkatu 8, 15110 Lahti"
+    }],
+    basketball:[{
+        title: "Organizers",
+        content: "LAB University of Applied Sciences together with Lahden Ahkera ry."  
+    },
+    {
+        title: "Time and place",
+        content: "-May 16  – 17, 2020\n-Lahti Sports Centre\n-Salpausselänkatu 8, 15110 Lahti"
+    }],
+  
+}
